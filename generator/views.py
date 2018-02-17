880fa68044014c2e4959222e4db3e700e9abddfc
tra38 <tra38@nau.edu>
2018-02-16 22:07:34 -0600

    Create /profile and /email RESTful endpoints
    /profile
    Inputs:
     - recruiter_file (PDF)
     - candidate_file (PDF)
    Output:
     - recruiter_json
     - candidate_json
     - generated_text
    
    /email
    Inputs:
     - recruiter_json
     - candidate_json
    Output:
     - recruiter_json (echo)
     - candidate_json (echo)
     - generated_text

--- generator/views.py
+++ generator/views.py
@@ -7,6 +7,12 @@ import generator.resume_saver as resume_saver
 
 import generator.letter_generator as letter_generator
 
+from django.http import JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+
 def index(request):
   context = {}
   if request.method == 'POST':
@@ -20,14 +26,31 @@ def index(request):
         context["uploaded_file_json_2"] = json_blob_candidate.json
       if context["uploaded_file_json"] and context["uploaded_file_json_2"]:
         context["generated_text"] = letter_generator.generate(context["uploaded_file_json"], context["uploaded_file_json_2"])
-
-
   return render(request, 'generator/index.html', context)
 
+@csrf_exempt
 def profile(request):
   context = {}
-  return render(request, 'generator/profile.html', context)
+  if request.FILES['recruiter_file']:
+    recruiter_file = request.FILES['recruiter_file']
+    json_blob_recruiter = resume_saver.save(recruiter_file)
+    context["recruiter_json"] = json_blob_recruiter.json
+  if request.FILES['candidate_file']:
+    candidate_file = request.FILES['candidate_file']
+    json_blob_candidate = resume_saver.save(candidate_file)
+    context["candidate_json"] = json_blob_candidate.json
+  if context["recruiter_json"] and context["candidate_json"]:
+    context["generated_text"] = letter_generator.generate(context["recruiter_json"], context["candidate_json"])
+  return JsonResponse(context)
 
+@csrf_exempt
 def email(request):
   context = {}
-  return render(request, 'generator/email.html', context)
+  data = json.loads(request.body)
+  if data['recruiter_json']:
+    context["recruiter_json"] = data['recruiter_json']
+  if data['candidate_json']:
+    context["candidate_json"] = data['candidate_json']
+  if context["recruiter_json"] and context["candidate_json"]:
+    context["generated_text"] = letter_generator.generate(context["recruiter_json"], context["candidate_json"])
+  return JsonResponse(context)
